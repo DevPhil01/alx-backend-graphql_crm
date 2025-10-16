7f6897c8201cc2084a7c2cdc860eabd77b474dc7
Philip Oyoo <philipoyoo11@gmail.com>
2025-10-16 17:39:57 +0300

    Refactor GraphQL schema to include mutations

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,10 +1,13 @@
 import graphene
-
-class CRMQuery(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
 class Query(CRMQuery, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
 
