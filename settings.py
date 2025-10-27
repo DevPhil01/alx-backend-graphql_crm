a9b84ab2792297c20e08899af14aafce60519654
Philip Oyoo <philipoyoo11@gmail.com>
2025-10-27 12:50:41 +0300

    Add cron job for CRM heartbeat logging
    Added cron job to log CRM heartbeat every 5 minutes.

--- settings.py
+++ settings.py
@@ -103,7 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+]
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -125,5 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
